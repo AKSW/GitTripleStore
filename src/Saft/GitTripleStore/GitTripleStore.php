e48d34f8bbe18e82a4ea18905d589b073fbe4d98
Oliver Skawronek <oskawron@imn.htwk-leipzig.de>
2015-02-17 18:02:48 +0100

    resetted functionality. every method throws now a 'not implemented' exception

--- src/Saft/GitTripleStore/GitTripleStore.php
+++ src/Saft/GitTripleStore/GitTripleStore.php
@@ -6,6 +6,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Exception;
 use LogicException;
+use InvalidArgumentException;
 use Filicious\Filesystem;
 use Filicious\Local\LocalAdapter;
 use Filicious\File;
@@ -14,21 +15,16 @@ final class GitTripleStore extends AbstractPatternFragmentTripleStore
 {
     private $log;
     private $config;
-    private $fileSystem;
-    private $graphManager;
     private $initialized;
 
     public function __construct(Config $config)
     {
         if (is_null($config)) {
-            throw new \InvalidArgumentException('$config is null');
+            throw new InvalidArgumentException('$config is null');
         }
         $this->log = new Logger('GitTripleStore');
         $this->log->pushHandler(new StreamHandler('php://output'));
         $this->config = $config;
-        $this->fileSystem = new Filesystem(new LocalAdapter(
-            $config->workingDirectory));
-        $this->graphManager = new GraphManager($config->workingDirectory);
         $this->initialized = false;
     }
 
@@ -36,17 +32,7 @@ final class GitTripleStore extends AbstractPatternFragmentTripleStore
         if ($this->initialized) {
             return;
         }
-        
-        $this->getOrCreateWorkingDirectory();
-        $storeFile = $this->fileSystem->getFile('.store');
-        if ($storeFile->exists()) {
-            $this->graphManager->loadMapping($storeFile);
-            $this->log->info('.store file loaded');
-        } else {
-            $this->graphManager->saveMapping($storeFile);
-            $this->log->info('Created .store file');
-        }
-        
+
         $this->initialized = true;
         $this->log->info('Initialized');
     }
@@ -73,50 +59,28 @@ final class GitTripleStore extends AbstractPatternFragmentTripleStore
         throw new Exception('Method not implemented');
     }
 
-    public function addMultipleStatements(array $Statements, $graphUri = null, array $options = array())
+    public function addMultipleStatements(array $statements, $graphUri = null, array $options = array())
     {
         throw new Exception('Method not implemented');
     }
 
-    public function deleteMultipleStatements(array $Statements, $graphUri = null)
+    public function deleteMultipleStatements(array $statements, $graphUri = null)
     {
         throw new Exception('Method not implemented');
     }
 
-    public function getMatchingStatements(array $Statements, $graphUri = null, array $options = array())
+    public function getMatchingStatements(array $statements, $graphUri = null, array $options = array())
     {
         throw new Exception('Method not implemented');
     }
 
-    public function hasMatchingStatement($Statement, $graphUri = null)
+    public function hasMatchingStatement(array $statements, $graphUri = null)
     {
         throw new Exception('Method not implemented');
     }
-
-    private function getOrCreateWorkingDirectory() {
-        $workingDir = $this->fileSystem->getFile('.');
-        if ($workingDir->exists()) {
-            $this->ensureWorkingDirReadability();
-            return $workingDir;
-        }
-        $workingDir->createDirectory(true);
-        $this->log->info('Created working directory');
-        if (!$workingDir->exists()) {
-            throw new Exception('Unable to create working directory');
-        } else if (!$workingDir->isReadable()) {
-            throw new Exception('Created working directory is not readable');
-        }
-        return $workingDir;
-    }
-
-    private function ensureWorkingDirReadability() {
-        $workingDir = $this->fileSystem->getFile('.');
-        if (!$workingDir->exists()) {
-            throw new Exception('Working Directory does not exist');
-        } else if (!$workingDir->isDirectory()) {
-            throw new Exception('Working Directory is not directory');
-        } else if (!$workingDir->isReadable()) {
-            throw new Exception('Working Directory is not readable');
-        }
+    
+    public function featureSupported()
+    {
+        throw new Exception('Method not implemented');
     }
 }
